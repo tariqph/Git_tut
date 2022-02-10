05419eea6ae0de2d8a2dff3f20032de2fc367d1e
tariqph <tariqanwarph@gmail.com>
2022-02-10 11:25:26 +0530

    second commit

--- src/app.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('Data/test.csv')
-
-print(data.head())
---
+++ src/test.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+data = pd.read_csv('G:\DS - Competitions and projects\Git_tut\Data\\test.csv')
+
+print(data.head())
+
